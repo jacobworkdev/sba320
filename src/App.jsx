284142342e6b9ee14bb30485bacd912538c82ac9
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-02-19 06:31:18 -0600

    added Routes and router.

--- src/App.jsx
+++ src/App.jsx
@@ -67,10 +67,19 @@ const Home = () => {
     </div>
   );
 
-
+//app routes
   function App(){
     return(
-      <Router></Router>
+      <Router>
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </Router>
     )
   }
 
