1e7bf7200b080c45570b042d4aabad7c3816f3ae
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-02-19 06:26:23 -0600

    added states

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+
+//states
+const Home = () => {
+  const [width, setWidth] = useState(400);
+  const [height, setHeight] = useState(400);
+  const [grayscale, setGrayscale] = useState(false);
+  const [blur, setBlur] = useState(0);
+  const [seed, setSeed] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+}
